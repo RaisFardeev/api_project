f44309e0d971ee12369573b63f0c70da2d27e2b8
RaisFardeev <raisfardeev5@gmail.com>
2024-09-19 19:53:05 +0300

    ! views + documentation and annotations

--- main/serializers.py
+++ main/serializers.py
@@ -1,17 +1,33 @@
 from rest_framework import serializers
-from .models import Product
+from main.models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор модели Product.
+    Позволяет валидировать цену и название.
+    """
     class Meta:
         model = Product
         fields = '__all__'
 
-    def validate_price(self, value):
+    def validate_price(self, value: int or float) -> int or float:
+        """
+        Проверка поля цена
+        :param value: Цена продукта
+        :raise: serializers.ValidationError, если цена неположительна
+        :return: Цена продукта
+        """
         if value <= 0:
             raise serializers.ValidationError("Цена должна быть положительной.")
         return value
 
-    def validate_name(self, value):
+    def validate_name(self, value :str) -> str:
+        """
+        Проверка поля название на наличие символов
+        :param value: Название продукта
+        :raise: serializers.ValidationError, если название пустое
+        :return: Название продукта
+        """
         if not value.strip():
             raise serializers.ValidationError("Название не должно быть пустым.")
         return value
--- main/views.py
+++ main/views.py
@@ -1,28 +1,43 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
 from rest_framework import status
 from main.models import Product
 from main.serializers import ProductSerializer
 
 
-
-def main_page(request):
+def main_page(request: HttpRequest) -> HttpResponse:
+    """
+    Главная страница
+    :param request: Объект HTTP-запроса
+    :return: HTTP-ответ с рендером шаблона main_page.html
+    """
     return render(request, 'main_page.html')
 
 
 @api_view(['GET'])
-def product_list(request):
+def product_list(request: Request) -> Response:
+    """
+    Вывод всех продуктов
+    :param request: Объект DRF запроса
+    :return: JSON response содержащий список продуктов.
+    """
     products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
+    serializer: Serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
 
 @api_view(['POST'])
-def product_create(request):
-    serializer = ProductSerializer(data=request.data)
-    products = Product.objects.all()
-    products.delete()
+def product_create(request: Request) -> Response:
+    """
+    Создание нового продукта
+    :param request: Объект DRF запроса содержащий информацию о продукте
+    :return: JSON response содержащий информацию о продукте или ошибки при валидации.
+    """
+    serializer: Serializer = ProductSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
